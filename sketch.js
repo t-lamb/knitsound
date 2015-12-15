6803a3be21a574a1263bc5a510f52079cf6000c5
Teresa Lamb <flour1337@gmail.com>
2015-12-15 11:07:53 -0500

    working with 2

--- sketch.js
+++ sketch.js
@@ -1,10 +1,16 @@
 var formResolution = 5; //how many points in shape
 var centerX, centerY;
+var centerX2;
 var startRadius = 100;
 var stepSize = 1;
+var stepSize2 = 1;
 var x = [formResolution];
 var y = [formResolution];
-var touching;
+var x2 = [formResolution];
+var y2 = [formResolution];
+var touching1;
+var touching2;
+var h1, h2, s, b;
 
 var socket = io.connect('http://45.55.43.235:1027/');
 
@@ -13,8 +19,16 @@ socket.on('connect', function(){
 });
   
 socket.on('sensor', function(data){
-    console.log("needles!" + data);
-    touching = true;
+    //console.log("needles!" + data);
+    if (data == "1") {
+        touching1 = true;
+        console.log("needles1:" + data);
+    }
+    if (data == "2") {
+        touching2 = true;
+        console.log("needles2:" + data);
+    }
+    
 });
 
 function setup() {
@@ -22,54 +36,95 @@ function setup() {
     //playMode(restart);
 
 	createCanvas(windowWidth, windowHeight);
-      var angle = radians(360/formResolution);
+        var angle = radians(360/formResolution);
 
-      for (var i = 0; i < formResolution; i++) {
-            x[i] = cos(angle*i) * startRadius;
-            y[i] = sin(angle*i) * startRadius;
-      }
+          for (var i = 0; i < formResolution; i++) {
+                x[i] = cos(angle*i) * startRadius;
+                y[i] = sin(angle*i) * startRadius;
+          }
 
-      //random color stroke
-      colorMode(HSB, 360, 100, 100, 1);
-      var h = random(360);
-      var s = 70;
-      var b = 100;
-      stroke(h, s, b);
-      strokeWeight(5);
-      fill('#112336');
+          for (var i = 0; i < formResolution; i++) {
+                x2[i] = cos(angle*i) * startRadius;
+                y2[i] = sin(angle*i) * startRadius;
+          }
+
+          //random color stroke
+          colorMode(HSB, 360, 100, 100, 1);
+          h1 = random(360);
+          h2 = random(360);
+          s = 70;
+          b = 100;
+          //stroke(h1, s, b);
+          strokeWeight(5);
+          noFill();
+          centerX = windowWidth/4;
+          centerY = windowHeight/2;
+          centerX2 = 3 * windowWidth/4;
+
 }
 
 function draw() {
 	background('#112336');
-        centerX = mouseX;
-    centerY = mouseY;
-    for (var i=0; i<formResolution; i++) {
-        x[i] += random(-stepSize, stepSize);
-        y[i] += random(-stepSize, stepSize);
-    }
-
-    beginShape();   
-    // start controlpoint
-    curveVertex(x[formResolution-1]+centerX, y[formResolution-1]+centerY);
-
-    // only these points are drawn
-    for (var i=0; i<formResolution; i++) {
-        curveVertex(x[i]+centerX, y[i]+centerY);
-    }
     
-    curveVertex(x[0]+centerX, y[0]+centerY);
+        //shape 1
+        for (var i=0; i<formResolution; i++) {
+            x[i] += random(-stepSize, stepSize);
+            y[i] += random(-stepSize, stepSize);
+        }
+        stroke(h1, s, b);
+        beginShape();   
+        // start controlpoint
+        curveVertex(x[formResolution-1]+centerX, y[formResolution-1]+centerY);
 
-    // end controlpoint
-    curveVertex(x[1]+centerX, y[1]+centerY);
-    endShape();
+        // only these points are drawn
+        for (var i=0; i<formResolution; i++) {
+            curveVertex(x[i]+centerX, y[i]+centerY);
+        }
+        
+        curveVertex(x[0]+centerX, y[0]+centerY);
+
+        // end controlpoint
+        curveVertex(x[1]+centerX, y[1]+centerY);
+        endShape();
+
+        //shape2
+        for (var i=0; i<formResolution; i++) {
+            x2[i] += random(-stepSize2, stepSize2);
+            y2[i] += random(-stepSize2, stepSize2);
+        }
+
+        stroke(h2, s, b);
+        beginShape();
+
+        // start controlpoint
+        curveVertex(x2[formResolution-1]+centerX2, y2[formResolution-1]+centerY);
+
+        // only these points are drawn
+        for (var i=0; i<formResolution; i++) {
+            curveVertex(x2[i]+centerX2, y2[i]+centerY);
+        }
+        
+        curveVertex(x2[0]+centerX2, y2[0]+centerY);
+
+        // end controlpoint
+        curveVertex(x2[1]+centerX2, y2[1]+centerY);
+        endShape();
+        
   
 
-    if (mouseIsPressed) {
+    if (mouseIsPressed || touching1) {
         stepSize = 20;
         song.play();
 
     } else {
         stepSize = 3;
+    }
+
+    if (touching2){
+        stepSize2 = 20;
+
+    } else {
+        stepSize2 = 3;
         //song.stop();
         //song.pause();
     }
